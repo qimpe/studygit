fd8283c243fc998257b1d755e618b400cf3467cf
Krasnov Daniil <krasndvs2@gmail.com>
2024-03-16 14:05:01 +0300

    init commit

--- StudyBuddyRemake/StudyBuddy/postsapp/urls.py
+++ StudyBuddyRemake/StudyBuddy/postsapp/urls.py
@@ -2,6 +2,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.mainpage, name='postslink'),
-    path('<int:pk>', views.postsDetailView.as_view(), name='posts-detail')
+    path("", views.mainpage, name="postslink"),
+    path("<int:pk>", views.postsDetailView.as_view(), name="posts-detail"),
 ]
