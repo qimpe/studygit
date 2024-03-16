f96442c2f9d7715456bc72e5d05682bc4e63908e
Krasnov Daniil <krasndvs2@gmail.com>
2024-03-16 13:38:39 +0300

    add bookmark

--- StudyBuddyRemake/StudyBuddy/homepage/views.py
+++ StudyBuddyRemake/StudyBuddy/homepage/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 
+
 # Create your views here.
 def homepagefunc(request):
-    return render(request, 'homepagetemplate.html')
+    return render(request, "homepagetemplate.html")
