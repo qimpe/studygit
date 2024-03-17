da601ab47a7d1cf1c2579fdab47026a18f35c9a8
Kvasov Arseniy <arsenijtam@gmail.com>
2024-03-17 20:18:11 +0300

    login/logout after the 62 lesson

--- StudyBuddyRemake/StudyBuddy/usersapp/urls.py
+++ StudyBuddyRemake/StudyBuddy/usersapp/urls.py
@@ -6,5 +6,5 @@ app_name = "users"
 
 urlpatterns = [
     path("login/", views.LoginUser.as_view(), name="login"),
-    path("logout/", views.LogoutView.as_view(), name="logout"),
+    path("logout/", LogoutView.as_view(), name="logout"),
 ]
