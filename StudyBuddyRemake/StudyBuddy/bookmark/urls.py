806b7fad079d8b75fa35a9ea503e0230b91dd037
Krasnov Daniil <krasndvs2@gmail.com>
2024-03-20 19:13:03 +0300

    add func add_to_favorite

--- StudyBuddyRemake/StudyBuddy/bookmark/urls.py
+++ StudyBuddyRemake/StudyBuddy/bookmark/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import bookmark
+from .views import bookmark, add_to_favorite
 
 urlpatterns = [
     path("", bookmark, name="bookmark"),
+    path("add-to-favorite/<int:post_id>/", add_to_favorite, name="add_to_favorite"),
 ]
--- StudyBuddyRemake/StudyBuddy/bookmark/views.py
+++ StudyBuddyRemake/StudyBuddy/bookmark/views.py
@@ -1,8 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
-
+from .models import FavoritePost
+from postsapp.models import Post
 # Create your views here.
 
 
 def bookmark(request):
-    return render(request, "bookmark.html")
+    favorite_posts = (
+        FavoritePost.objects.all()
+        .filter(user_id=request.user.id)
+        .values("user_id", "post_id")
+    )
+    favorite_posts_dict = {}
+    post_ids = []
+    for post in favorite_posts.values():
+        post_ids.append(post["post_id"])
+
+    favorite_posts_dict["posts"] = post_ids
+    print(favorite_posts_dict)
+    for post in favorite_posts_dict.items():
+        print(post[1])
+    return render(request, "bookmark.html", favorite_posts_dict)
+    """favorite_posts = FavoritePost.objects.all().filter(user_id=request.user.id)
+    post_ids = favorite_posts.values_list("post_id", flat=True)
+    posts = Post.objects.all().filter(id__in=post_ids)
+    print(posts)"""
+
+
+def add_to_favorite(request, post_id=0):
+    post = get_object_or_404(Post, id=post_id)
+    favorite_post = FavoritePost.objects.create(user=request.user, post=post)
+    favorite_post.save()
+    return redirect("posts-detail", pk=post_id)
+
+
+def delete_from_favorite(request, post_id=0): ...
