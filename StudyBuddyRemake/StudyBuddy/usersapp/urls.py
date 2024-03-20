7cb7e76eb3a20986c7bf14508d7a3ff8a451efd2
Kvasov Arseniy <arsenijtam@gmail.com>
2024-03-20 21:37:19 +0300

    success registration with no email

--- StudyBuddyRemake/StudyBuddy/usersapp/urls.py
+++ StudyBuddyRemake/StudyBuddy/usersapp/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path, include
 from . import views
-from django.contrib.auth.views import LogoutView
 
 app_name = "users"
 
 urlpatterns = [
     path("login/", views.LoginUser.as_view(), name="login"),
-    path("logout/", LogoutView.as_view(), name="logout"),
+    path("logout/", views.logout_user, name="logout"),
 ]
