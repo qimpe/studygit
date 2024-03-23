81fce73d6a840d4b617a8955e0ff08f632d678c0
Kvasov Arseniy <arsenijtam@gmail.com>
2024-03-23 23:21:17 +0300

    bookmarks, final version, not good cancel

--- StudyBuddyRemake/StudyBuddy/StudyBuddy/settings.py
+++ StudyBuddyRemake/StudyBuddy/StudyBuddy/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    "bookmark",
+
     "homepage",
     "postsapp",
     "usersapp",
--- StudyBuddyRemake/StudyBuddy/postsapp/urls.py
+++ StudyBuddyRemake/StudyBuddy/postsapp/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path, include
 from . import views
+from .views import add_to_favorites, del_from_favorites, bookmarks
 
 urlpatterns = [
     path("", views.mainpage, name="postslink"),
     path("<int:pk>", views.postsDetailView.as_view(), name="posts-detail"),
-    path('create/', views.create, name="create")
+    path('create/', views.create, name="create"),
+    path('post/<int:post_id>/add_to_favorites/', add_to_favorites, name='add_to_favorites'),
+    path('post/<int:post_id>/del_from_favorites/', del_from_favorites, name='del_from_favorites'),
+    path('bookmarks', bookmarks, name='bookmarks')
 ]
